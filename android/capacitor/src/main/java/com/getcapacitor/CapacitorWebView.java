e78283c0da485a379ac502c77d4a505ae015917b
John Hayford <jhayford@mapsysinc.com>
2024-02-09 10:41:55 -0500

    doing bad things

--- android/capacitor/src/main/java/com/getcapacitor/CapacitorWebView.java
+++ android/capacitor/src/main/java/com/getcapacitor/CapacitorWebView.java
@@ -1,12 +1,22 @@
 package com.getcapacitor;
 
+import static android.text.InputType.TYPE_CLASS_NUMBER;
+
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.InputType;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
+import android.view.View;
+import android.view.accessibility.AccessibilityNodeInfo;
 import android.view.inputmethod.BaseInputConnection;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
 import android.webkit.WebView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 public class CapacitorWebView extends WebView {
 
@@ -29,16 +39,23 @@ public class CapacitorWebView extends WebView {
         } else {
             config = CapConfig.loadDefault(getContext());
         }
+       boolean captureInput = config.isInputCaptured();
 
-        boolean captureInput = config.isInputCaptured();
+        SharedPreferences prefs = getPrefs();
+        String name = prefs.getString("name", "");
+        String type = prefs.getString("type", "");
+        if ("tel".equals(type) || "number".equals(type)){
+            outAttrs.inputType = TYPE_CLASS_NUMBER;
+        }
         if (captureInput) {
             if (capInputConnection == null) {
-                capInputConnection = new BaseInputConnection(this, false);
+                 capInputConnection = new BaseInputConnection(this, false);
             }
             return capInputConnection;
         }
         return super.onCreateInputConnection(outAttrs);
     }
+
 
     @Override
     @SuppressWarnings("deprecation")
@@ -47,6 +64,13 @@ public class CapacitorWebView extends WebView {
             evaluateJavascript("document.activeElement.value = document.activeElement.value + '" + event.getCharacters() + "';", null);
             return false;
         }
+        if (event.getKeyCode()== KeyEvent.KEYCODE_NAVIGATE_NEXT && event.getAction()== KeyEvent.ACTION_DOWN)
+        {
+            return super.dispatchKeyEvent(new KeyEvent(KeyEvent.KEYCODE_ENTER,KeyEvent.ACTION_DOWN));
+        }
         return super.dispatchKeyEvent(event);
+    }
+    private SharedPreferences getPrefs(){
+        return getContext().getSharedPreferences("controltex.activeelement", Context.MODE_PRIVATE);
     }
 }
